cf380605a094b5bad988fa19579628840e22694b
asrinivasan75 <80648675+asrinivasan75@users.noreply.github.com>
2022-07-21 21:43:18 -0400

    v0.1.1
    Added leaderboard function, changed stylesheets, and added restart button.

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const quoteDisplayElement = document.getElementById('quoteDisplay')
 const quoteInputElement = document.getElementById('quoteInput')
 const timerElement = document.getElementById('timer')
 const wpmElement = document.getElementById('wpmtext')
+const leaderboardOne = document.getElementById('leaderboardone')
+const leaderboardTwo = document.getElementById('leaderboardtwo')
+const leaderboardThree = document.getElementById('leaderboardthree')
+const leaderboardFour = document.getElementById('leaderboardfour')
+const leaderboardFive = document.getElementById('leaderboardfive')
+if (localStorage.getItem("leaderboard1") === null) {
+    topScores = [0, 0, 0, 0, 0]
+}
+else {
+    topScores = []
+    topScores[0] = window.localStorage.getItem('leaderboard1')
+    topScores[1] = window.localStorage.getItem('leaderboard2')
+    topScores[2] = window.localStorage.getItem('leaderboard3')
+    topScores[3] = window.localStorage.getItem('leaderboard4')
+    topScores[4] = window.localStorage.getItem('leaderboard5')
+}
+
 quoteInputElement.addEventListener('input', () => {
     const arrayQuote = quoteDisplayElement.querySelectorAll('span')
     const arrayValue = quoteInputElement.value.split('')
@@ -46,7 +63,26 @@ quoteInputElement.addEventListener('input', () => {
     })
 
     if (correct){
+        let finalWPM = getWPM()
+        for (var i = 0; i < topScores.length; i++){
+            if (finalWPM > topScores[i]){
+                temphigher = finalWPM
+                for (var j = i; j < topScores.length; j++){
+                    temp = topScores[j]
+                    topScores[j] = temphigher
+                    temphigher = temp
+                }
+                temp = topScores[i]
+                topScores[i] = finalWPM
+                break
+            }
+        }
         correctWords = 0;
+        window.localStorage.setItem('leaderboard1', topScores[0])
+        window.localStorage.setItem('leaderboard2', topScores[1])
+        window.localStorage.setItem('leaderboard3', topScores[2])
+        window.localStorage.setItem('leaderboard4', topScores[3])
+        window.localStorage.setItem('leaderboard5', topScores[4])
         renderNewQuote()
     }
 })
@@ -58,6 +94,7 @@ function getRandomQuote() {
 
 async function renderNewQuote() {
     correctWords = 0
+    setTextScores()
     const quote = await getRandomQuote()
     quotelist = Array.from(quote)
     quoteDisplayElement.innerHTML = ''
@@ -73,33 +110,43 @@ async function renderNewQuote() {
 
 let startTime
 function startTimer() {
-    timerElement.innerText = 0
+    timerElement.innerText = "Timer: 0 seconds"
     startTime = new Date()
     setInterval(() => {
         if (Math.floor(getTimerTime()) == 1){
-            timerElement.innerText = Math.floor(getTimerTime()) + ' second'
+            timerElement.innerText = 'Timer: ' + Math.floor(getTimerTime()) + ' second'
         }
         else{
-            timerElement.innerText = Math.floor(getTimerTime()) + ' seconds'
+            timerElement.innerText = 'Timer: ' + Math.floor(getTimerTime()) + ' seconds'
         }
     }, 1000)
 }
 
 function startWPM() {
-    wpmElement.innerText = '0 wpm'
     setInterval(() => {
-        wpmElement.innerText = getWPM()
+        wpmElement.innerText = getWPM() + " wpm"
     }, 1000)
 
 }
 
+function setTextScores() { 
+    leaderboardOne.innerText = '1. ' + topScores[0] + ' WPM'
+    leaderboardTwo.innerText = '2. ' + topScores[1] + ' WPM'
+    leaderboardThree.innerText = '3. ' + topScores[2] + ' WPM'
+    leaderboardFour.innerText = '4. ' + topScores[3] + ' WPM'
+    leaderboardFive.innerText = '5. ' + topScores[4] + ' WPM'
+}
+
+
 function getWPM() {
     if (getTimerTime() == 0){
-        return '0 wpm'
+        return 0
     }
-    return Math.floor(correctWords * 60 / getTimerTime()) + ' wpm'
+    return Math.floor(correctWords * 60 / getTimerTime())
 }
 function getTimerTime() {
     return (new Date() - startTime) / 1000
 }
+
+
 renderNewQuote()
